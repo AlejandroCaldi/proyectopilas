7403f2ecff6601a78f6390f4e1ca37f5d6905d73
curso07 <alejandro.cald@gmail.com>
2025-01-01 16:50:45 +0100

    Cambios es nombres para mayor claridad. Borrado de código comentado.

--- src/main/java/es/santander/ascender/Pila.java
+++ src/main/java/es/santander/ascender/Pila.java
@@ -2,24 +2,24 @@ package es.santander.ascender;
 
 public class Pila {
 
-    private Nodo raiz;
+    private Nodo topePila;
 
     public void add(String mensaje) {
-        if (raiz == null) {
-            this.raiz = new Nodo(mensaje);
+        if (topePila == null) {
+            this.topePila = new Nodo(mensaje);
         } else {
             Nodo nodo = new Nodo(mensaje);
-            nodo.setAnterior(raiz);
-            raiz = nodo;
+            nodo.setAnterior(topePila);
+            topePila = nodo;
         }
     }
 
     public String get() {
-        if (raiz == null) {
+        if (topePila == null) {
             return null;
         } 
-        String valor = raiz.getValor();
-        raiz = raiz.getAnterior();
+        String valor = topePila.getValor();
+        topePila = topePila.getAnterior();
         System.gc();
         return valor;
 
--- src/test/java/es/santander/ascender/pilaTest.java
+++ src/test/java/es/santander/ascender/pilaTest.java
@@ -7,20 +7,13 @@ import org.junit.jupiter.api.Test;
 
 public class pilaTest {
     @Test
-    void testAdd() {
+    void testAddyGeT() {
         Pila pila = new Pila();
 
 
         pila.add("primera");
         pila.add("segunda");
         pila.add("tercera");
-
-        // String siguiente = null;
-        // int cuenta = 0;
-        // while((siguiente = pila.get()) != null) {
-        //     cuenta++;
-        // }
-        // assertEquals(3, cuenta);
 
         assertEquals("tercera", pila.get());
         assertEquals("segunda", pila.get());
